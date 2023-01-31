06af48062c0719c99d755d13323acef7701eb681
Ioannis Skottis <giannis.sc@gmail.com>
2023-01-31 16:37:58 +0000

    Refactoring of type names

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub mod panning;
 pub mod zooming;
 pub mod snapping;
 pub mod rotation;
-// pub mod canvas;
+pub mod canvas;
 pub mod save_system;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,46 +32,46 @@ pub const REQUEST_PAINT: Selector =  Selector::new("request-paint");
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 /// 
-/// GridNodePosition
+/// GridIndex
 /// 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 #[derive(Clone, Data, Copy, PartialEq, Debug, Hash, Eq)]
-pub struct GridNodePosition {
+pub struct GridIndex {
     pub row: usize,
     pub col: usize,
 }
 
-impl GridNodePosition {
-    pub fn above(self) -> GridNodePosition {
-        GridNodePosition {
+impl GridIndex {
+    pub fn above(self) -> GridIndex {
+        GridIndex {
             row: self.row - 1,
             col: self.col,
         }
     }
 
-    pub fn below(self) -> GridNodePosition {
-        GridNodePosition {
+    pub fn below(self) -> GridIndex {
+        GridIndex {
             row: self.row + 1,
             col: self.col,
         }
     }
 
-    pub fn left(self) -> GridNodePosition {
-        GridNodePosition {
+    pub fn left(self) -> GridIndex {
+        GridIndex {
             row: self.row,
             col: self.col - 1,
         }
     }
 
-    pub fn right(self) -> GridNodePosition {
-        GridNodePosition {
+    pub fn right(self) -> GridIndex {
+        GridIndex {
             row: self.row,
             col: self.col + 1,
         }
     }
 
     // Also known in vlsi as the Manhattan Architecture
-    pub fn neighbors_rectilinear(self) -> [GridNodePosition; 4] {
+    pub fn neighbors_rectilinear(self) -> [GridIndex; 4] {
         let above = self.above();
         let below = self.below();
         let left = self.left();
@@ -80,7 +80,7 @@ impl GridNodePosition {
     }
 
     // Also known in vlsi as the X Architecture
-    pub fn neighbors_diagonal(self) -> [GridNodePosition; 4] {
+    pub fn neighbors_diagonal(self) -> [GridIndex; 4] {
         let above = self.above();
         let below = self.below();
         let above_left = above.left();
@@ -133,7 +133,6 @@ pub enum GridAction{
     Add,
     Remove,
     Move,
-    Panning,
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -143,16 +142,16 @@ pub enum GridAction{
 //////////////////////////////////////////////////////////////////////////////////////
 #[derive(Clone, PartialEq, Data, Debug, Hash, Eq)]
 pub enum StackItem<T: GridItem>{
-    Add(GridNodePosition, T, Option<T>),
-    Remove(GridNodePosition, T),
-    Move(GridNodePosition, GridNodePosition, T),
-    BatchAdd(HashMap<GridNodePosition, (T, Option<T>)>),
-    BatchRemove(HashMap<GridNodePosition, T>),
+    Add(GridIndex, T, Option<T>),
+    Remove(GridIndex, T),
+    Move(GridIndex, GridIndex, T),
+    BatchAdd(HashMap<GridIndex, (T, Option<T>)>),
+    BatchRemove(HashMap<GridIndex, T>),
 }
 
 impl<T: GridItem> StackItem<T>{
-    fn get_positions(&self) -> HashSet<GridNodePosition>{
-        let mut set: HashSet<GridNodePosition> = HashSet::new();
+    fn get_positions(&self) -> HashSet<GridIndex>{
+        let mut set: HashSet<GridIndex> = HashSet::new();
         
         match self{
             StackItem::Add(pos, _, _) => {set.insert(*pos);},
@@ -175,7 +174,7 @@ impl<T: GridItem> StackItem<T>{
         set
     }
 
-    fn forward(&self, grid: &mut HashMap<GridNodePosition, T>){
+    fn forward(&self, grid: &mut HashMap<GridIndex, T>){
         match self{
             StackItem::Add(pos, current_item, _) => {grid.insert(*pos, *current_item);},
             StackItem::Remove(pos, _) => {grid.remove(pos);},
@@ -196,7 +195,7 @@ impl<T: GridItem> StackItem<T>{
         }
     }
 
-    fn reverse(&self, grid: &mut HashMap<GridNodePosition, T>){
+    fn reverse(&self, grid: &mut HashMap<GridIndex, T>){
         match self{
             StackItem::Add(pos, _, previous_item) => {
                 grid.remove(pos);
@@ -233,7 +232,7 @@ impl<T: GridItem> StackItem<T>{
 //////////////////////////////////////////////////////////////////////////////////////
 #[derive(Clone, PartialEq, Data, Lens)]
 pub struct GridWidgetData<T:GridItem + PartialEq>{
-    pub grid: HashMap<GridNodePosition, T>,
+    pub grid: HashMap<GridIndex, T>,
     pub save_system: SaveSystemData<StackItem<T>>,
     pub action: GridAction,
     pub grid_item: T,
@@ -249,7 +248,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
         }
     }
 
-    fn add_node(&mut self, pos: &GridNodePosition, item: T) -> bool{
+    fn add_node(&mut self, pos: &GridIndex, item: T) -> bool{
         let option = self.grid.get(pos);
 
         let command_item;
@@ -267,7 +266,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
         false
     }
 
-    fn remove_node(&mut self, pos: &GridNodePosition) -> bool{
+    fn remove_node(&mut self, pos: &GridIndex) -> bool{
         if let Some(item) = self.grid.remove(pos){
             if item.can_remove(){
                 let command_item = StackItem::Remove(*pos, item);
@@ -280,7 +279,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
         false
     }
 
-    fn move_node(&mut self, from: &GridNodePosition, to:&GridNodePosition) -> bool{
+    fn move_node(&mut self, from: &GridIndex, to:&GridIndex) -> bool{
         let item = self.grid.get(from).unwrap();
         let other = self.grid.get(to);
         if item.can_move(other) {
@@ -295,12 +294,12 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
 
     pub fn add_node_perimeter(
         &mut self,
-        pos: GridNodePosition,
+        pos: GridIndex,
         row_n: usize,
         column_n: usize,
         tool: T,
     ) {
-        let mut map: HashMap<GridNodePosition, (T, Option<T>)> = HashMap::new();
+        let mut map: HashMap<GridIndex, (T, Option<T>)> = HashMap::new();
         for row in pos.row..pos.row + row_n {
             //debug!("Add node perimeter");
             //debug!("Row: {:?}", row);
@@ -309,7 +308,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
                 //debug!("Printing top/bottom boundary");
                 for column in pos.col..pos.col + column_n {
                     map.insert(
-                        GridNodePosition {
+                        GridIndex {
                             row: row,
                             col: column,
                         },
@@ -320,7 +319,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
                 //debug!("Printing left/right boundary");
                 // Left Boundary
                 map.insert(
-                    GridNodePosition {
+                    GridIndex {
                         row: row,
                         col: pos.col,
                     },
@@ -328,7 +327,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
                 );
                 // Right Boundary
                 map.insert(
-                    GridNodePosition {
+                    GridIndex {
                         row: row,
                         col: pos.col + column_n - 1,
                     },
@@ -370,7 +369,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
         self.save_system.append(val_list);
     }
 
-    pub fn get_grid(&self) -> &HashMap<GridNodePosition, T> {
+    pub fn get_grid(&self) -> &HashMap<GridIndex, T> {
         &self.grid
     }
 
@@ -380,7 +379,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
     }
 
     pub fn clear_except(&mut self, set: HashSet<T>){
-        let mut map: HashMap<GridNodePosition, T> = HashMap::new();
+        let mut map: HashMap<GridIndex, T> = HashMap::new();
         for item_type in set {
             self.grid.retain(|pos, item|{
                 if *item == item_type {
@@ -395,7 +394,7 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
     }
 
     pub fn clear_only(&mut self, set: HashSet<T>){
-        let mut map: HashMap<GridNodePosition, T> = HashMap::new();
+        let mut map: HashMap<GridIndex, T> = HashMap::new();
         for item_type in set {
             self.grid.retain(|pos, item|{
                 if *item == item_type {
@@ -418,12 +417,17 @@ impl<T:GridItem + PartialEq> GridWidgetData<T>{
 /// 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// TODO: Keep as widget to perform scaling/translation of child children paint methods
+// TODO: Rename to CanvasWrapper
+// TODO: Move Snapping System out of main to lib and attach to Canvas
+// TODO: Replace grid with canvas
+// TODO: Translate event position to correct location based on scaling/translation offset
 
 #[derive(Clone, PartialEq, Data, Lens)]
 pub struct GridWidget<T> {
     cell_size: f64,
     phantom: PhantomData<T>,
-    start_pos: GridNodePosition,
+    start_pos: GridIndex,
     state: GridState,
     previous_stack_length: usize,
     previous_playback_index: usize,
@@ -434,24 +438,24 @@ impl<T> GridWidget<T> {
         GridWidget {
             cell_size,
             phantom: PhantomData,
-            start_pos: GridNodePosition { row: 0, col: 0 },
+            start_pos: GridIndex { row: 0, col: 0 },
             state: GridState::Idle,
             previous_stack_length: 0,
             previous_playback_index: 0,
         }
     }
 
-    fn grid_pos(&self, p: Point) -> Option<GridNodePosition> {
+    fn grid_pos(&self, p: Point) -> Option<GridIndex> {
         if p.x < 0.0 || p.y < 0.0 || self.cell_size == 0.0 {
             return None;
         }
         let col = (p.x / self.cell_size) as usize;
         let row = (p.y / self.cell_size) as usize;
 
-        Some(GridNodePosition { row, col })
+        Some(GridIndex { row, col })
     }
 
-    pub fn invalidation_area(&self, pos: GridNodePosition) -> Rect {
+    pub fn invalidation_area(&self, pos: GridIndex) -> Rect {
         let point = Point {
             x: self.cell_size * pos.col as f64,
             y: self.cell_size * pos.row as f64,
@@ -540,12 +544,6 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
                                             self.state = GridState::Running(data.action);
                                             data.action = GridAction::Remove;
                                         }
-                                    } else if e.button == MouseButton::Middle{
-                                        // info!("Middle Click");
-                                        if let GridAction::Dynamic = data.action{
-                                            self.state = GridState::Running(data.action);
-                                                data.action = GridAction::Panning;
-                                        }
                                     }
                                 }
 
@@ -554,8 +552,6 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
                                         if data.add_node(pos, data.grid_item) {
                                             change_tracker.insert(data.save_system.get_last_item().unwrap().clone());
                                         }
-                                    } else if data.action == GridAction::Panning {
-                                        self.start_pos = *pos;
                                     } else if data.action == GridAction::Remove && option.is_some(){
                                         if data.remove_node(pos) {
                                             change_tracker.insert(data.save_system.get_last_item().unwrap().clone());
@@ -600,9 +596,6 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
                                         }
                                     }        
                                 },
-                                GridAction::Panning => {
-                                    // Panning code to be completed
-                                },
                                 _ => (),
                             }
                         });
@@ -610,9 +603,6 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
         
                     Event::MouseUp(e) => {
                         if e.button == MouseButton::Right && self.state == GridState::Running(GridAction::Dynamic) && data.action == GridAction::Remove {
-                            self.state = GridState::Idle;
-                            data.action = GridAction::Dynamic;
-                        } else if e.button == MouseButton::Middle && self.state == GridState::Running(GridAction::Dynamic) && data.action == GridAction::Panning {
                             self.state = GridState::Idle;
                             data.action = GridAction::Dynamic;
                         } else if e.button == MouseButton::Left && self.state == GridState::Running(GridAction::Dynamic){
@@ -636,6 +626,7 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
             },
         }
 
+        // To be removed with refactoring
         if change_tracker.len() != 0 {           
             for item in &change_tracker {
                 for pos in item.get_positions().iter(){
@@ -689,7 +680,7 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
         // Draw grid cells
 
         let screen_space = ctx.size();
-        let damage_region  =ctx.region();
+        let damage_region = ctx.region().clone();
 
         // println!("Canvas Screen Space: {:?}", screen_space);
         // println!("Canvas Damage Region: {:?}\n", damage_region);
@@ -697,20 +688,16 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
         let visible_rows =  (screen_space.height / self.cell_size).ceil() as usize;
 
         // Calculate area to render
-        let mut paint_rectangles: Vector<Rect> = Vector::new();
-
-        for paint_rect in damage_region.rects().iter() {
-            paint_rectangles.push_front(*paint_rect);
-        }
+        let paint_rectangles = damage_region.rects();
 
         for paint_rect in paint_rectangles.iter() {
-            let from_grid_pos: GridNodePosition = self.grid_pos(paint_rect.origin()).unwrap();
+            let from_grid_pos: GridIndex = self.grid_pos(paint_rect.origin()).unwrap();
             let from_row = from_grid_pos.row;
             let from_col = from_grid_pos.col;
 
             let to_grid_pos = self
                 .grid_pos(Point::new(paint_rect.max_x(), paint_rect.max_y()))
-                .unwrap_or(GridNodePosition {
+                .unwrap_or(GridIndex {
                     col: visible_columns - 1,
                     row: visible_rows - 1,
                 });
@@ -731,7 +718,7 @@ impl<T:GridItem + PartialEq> Widget<GridWidgetData<T>> for GridWidget<T>{
                     };
                     let rect = Rect::from_origin_size(point, Size {width: self.cell_size, height: self.cell_size});
 
-                    let grid_pos = GridNodePosition { row, col };
+                    let grid_pos = GridIndex { row, col };
 
                     if let Some(runner) = data.grid.get(&grid_pos){
                         ctx.fill(rect, runner.get_color());
